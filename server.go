d59d75416c3cdd08784cb6bdae56ab2fe810f9a2
rob sullivan <rob@datachomp.com>
2015-09-12 21:46:14 -0500

    add suggestion routes

--- server.go
+++ server.go
@@ -13,11 +13,23 @@ func main() {
 
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", Index)
+    router.HandleFunc("/suggestions", SuggestionsIndex)
+    router.HandleFunc("/suggestions/{suggestionId}", SuggestionsShow)
 
     log.Println("Listening on localhost:8080...")
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    fmt.Fprintf(w, "Hello NerdBeer, %q", html.EscapeString(r.URL.Path))
   }
+
+func SuggestionsIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Suggestion Index!")
+}
+
+func SuggestionsShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    suggestionId := vars["suggestionId"]
+    fmt.Fprintln(w, "Suggestion show:", suggestionId)
+}
